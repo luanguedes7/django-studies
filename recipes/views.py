fcbcae9bd2f0dbf4cf8a35541b059d802935f26d
Luan Guedes <init.defaultBranch>
2024-06-04 17:48:57 -0300

    Using models in project of recipes 2

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
+from django.http import Http404
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published = True,
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published = True,
     ).order_by('-id')
-    
-    return render(request, 'recipes/pages/home.html', context={
+
+    if not recipes:
+        raise Http404('Not found! :(')
+
+    return render(request, 'recipes/pages/category.html', context={
             'recipes': recipes,
+            'title': f'{recipes.first().category.name} - Category' 
     })
 
 def recipe(request, id):
