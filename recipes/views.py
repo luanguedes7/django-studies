777d18db0db5411eba614a90ddad5b5bf86c134a
Luan Guedes <init.defaultBranch>
2024-06-03 22:00:01 -0300

    Making a little website to understand some concepts - Using inheritance in templates and template blocks 2

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes':[make_recipe() for _ in range(10)]
+    })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-template.html')
+    return render(request, 'recipes/pages/recipe-template.html', context={
+        'recipe':make_recipe(),
+        'is_detail_page': True
+    })
