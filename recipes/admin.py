00201d14178493f55fb8edd8406648e867d60451
Luan Guedes <init.defaultBranch>
2024-06-04 09:33:13 -0300

    Config models with admin.py and config urls

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Recipe
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Recipe)
