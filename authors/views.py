ea4e2425b6d7baf97c282032669eb5089f3cc3f1
Luan Guedes <init.defaultBranch>
2024-06-15 18:01:17 -0300

    Creating a new app to register users

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.http import Http404
 
 # Create your views here.
+def register_view(request):
+   register_form_data = request.session.get('register_form_data', None)
+   form = RegisterForm(register_form_data)
+   return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
+
+def register_create(request):
+   if not request.POST:
+      raise Http404()
+   
+   POST = request.POST
+   request.session['register_form_data'] = POST
+   form = RegisterForm(POST)
+
+   return redirect('register')
