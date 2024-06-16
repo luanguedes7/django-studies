6ad28fb38939cf8086dcf76093d36b31d254b620
Luan Guedes <init.defaultBranch>
2024-06-16 15:34:44 -0300

    fixing unencrypted password error

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -73,20 +73,22 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         url = reverse('create')
         response = self.client.post(url, data=self.form_data, follow=True)
         self.assertIn(msg, response.content.decode('utf-8'))
-
+    
     def test_username_field_min_length_should_be_4(self):
         self.form_data['username'] = 'joa'
-        url = reverse('authors:create')
+        url = reverse('create')
         response = self.client.post(url, data=self.form_data, follow=True)
 
         msg = 'Username must have at least 4 characters'
         self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('username'))
 
     def test_username_field_max_length_should_be_150(self):
         self.form_data['username'] = 'A' * 151
-        url = reverse('authors:create')
+        url = reverse('create')
         response = self.client.post(url, data=self.form_data, follow=True)
 
         msg = 'Username must have less than 150 characters'
 
         self.assertIn(msg, response.context['form'].errors.get('username'))
+        self.assertIn(msg, response.content.decode('utf-8'))
